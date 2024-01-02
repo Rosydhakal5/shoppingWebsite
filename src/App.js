4e9842938f45ae65bca2210f2cd94b623172c9f6
Rosy <rosydhakal333@gmail.com>
2024-01-02 16:15:29 +1100

    learning react by creating shoppingapp

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navigation from './nav';
+import Login from './Login';
+import './App.css';
+import './Login.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <>
+    <Navigation />
+    <Login />
+    </>
+   
+  );
+}
+
+export default App;
